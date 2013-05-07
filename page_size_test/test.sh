8c279a99e16fe7c61202aa856ffda43ccdb2744c
Steve Stagg <stestagg@gmail.com>
2013-05-08 00:45:55 +0100

    Turn constant expressions into defines to cope with obsoltete compilers

--- page_size_test/test.sh
+++ page_size_test/test.sh
@@ -8,7 +8,7 @@ cat field_template.pyx | sed s/!!!!/$1/ > bf.pyx
 
 cython -2 bf.pyx -o bf.c
 
-clang -bundle -Os -undefined dynamic_lookup -Wl,-F. -arch x86_64 bf.c \
+gcc -std=c99 -bundle -Os -undefined dynamic_lookup -Wl,-F. -arch x86_64 bf.c \
 	  -I/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
 	  -dynamic -fno-common -fno-strict-aliasing -fwrapv -mno-fused-madd -o bf.so 2>/dev/null
 
