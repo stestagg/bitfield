06e2cb600991a7a5b7b02942a407f6f3b92963b4
Steve Stagg <stestagg@gmail.com>
2013-05-08 16:20:42 +0100

    Add in code to test deployment using travis

---
+++ sdist_local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSION=$(env PYTHONPATH=. python -c 'import setup; print setup.VERSION')
+
+TEMP_DIR=$(mktemp -d)
+./setup.py sdist -d $TEMP_DIR
+
+ls $TEMP_DIR
+
+cp $TEMP_DIR/bitfield-$VERSION.tar.gz ./package.tar.gz
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import sys
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
+
+VERSION = "1.32"
 
 
 ext_modules = [
@@ -12,31 +13,32 @@ ext_modules = [
               ["cimpl/field.pyx"],
               extra_compile_args=["-g"],
               extra_link_args=["-g"],
-              depends=["cimpl/field.h"]
+              depends=["cimpl/field.h", "cimpl/field.c"]
               )
 ]
 
-setup(
-    name = "bitfield",
-    version = "1.31",
-    license = "BSD",
+if __name__ == "__main__":
+    setup(
+        name="bitfield",
+        version=VERSION,
+        license="BSD",
 
-    description = "A Cython fast compressed number set",
-    author = "Steve Stagg",
-    author_email = "stestagg@gmail.com",
+        description="A Cython fast compressed number set",
+        author="Steve Stagg",
+        author_email="stestagg@gmail.com",
 
-    url = "http://github.com/stestagg/bitfield",
+        url="http://github.com/stestagg/bitfield",
 
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+        ],
 
-    package_data = {"Cython": ["cimpl/*.pyx"]},
+        package_data={"Cython": ["cimpl/*.pyx"]},
 
-    cmdclass = {"build_ext": build_ext},
-    ext_modules = ext_modules,
-)
+        cmdclass={"build_ext": build_ext},
+        ext_modules=ext_modules,
+    )
