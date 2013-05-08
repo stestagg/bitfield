2b4127be6139bd3f972b2e2b403600d601b987a2
Steve Stagg <stestagg@gmail.com>
2013-05-08 15:18:39 +0100

    Further improvements to testing script, include test for small fields

--- page_size_test/test.py
+++ page_size_test/test.py
@@ -3,42 +3,72 @@ import time
 import sys
 
 
-def test():
-    ranges = ((1, 100), (102, 10000), (1000000, 2000000))
-    evil = range(0, 100000, 2)
+def timed(fun):
+    def wrapper():
+        num = 0
+        start = time.clock()
+        end = start
+        while end - start < 0.5:
+            fun()
+            end = time.clock()
+            num += 1
+        return (end - start) / num
+    return wrapper
 
-    start = time.clock()
-    ff = bf.Bitfield(ranges)
+
+RANGES = ((1, 100), (102, 10000), (1000000, 2000000))
+EVIL = range(0, 100000, 2)
+
+
+@timed
+def test():
+    ff = bf.Bitfield(RANGES)
     for i in xrange(0, 3000000, 10000):
         ff.add(i)
     gg = eval(repr(ff))
     assert len(gg - ff) == 0
     for i in ff:
         gg.discard(i)
-    aa = bf.Bitfield(evil)
+    aa = bf.Bitfield(EVIL)
     cc = ff - aa
     assert cc
     hh = bf.Bitfield()
     for i in xrange(0, 2000000, 2):
         hh.add(i)
-    stop = time.clock()
-    return stop - start
+
+
+@timed
+def test_small():
+    aa = bf.Bitfield()
+    bb = bf.Bitfield()
+    cc = bf.Bitfield()
+    for i in xrange(0, 100, 3):
+        aa.add(i)
+        bb.add(i + 1)
+        cc.add(i + 2)
+    assert len(aa) == len(bb) == len(cc)
+    assert len(aa | bb | cc) == 102
+    dd = bf.Bitfield(((500, 700),)) | (aa ^ bb ^ cc)
+    for i in dd:
+        i in aa
+        i in bb
+        i in cc
+        dd.remove(i)
+
 
 
 ONE_MILLION = 1000000
 SIZE = ONE_MILLION * 1000
 
 
+@timed
 def test2():
-    start = time.clock()
     field1 = bf.Bitfield([[0, SIZE]])
     field2 = bf.Bitfield([[SIZE, SIZE * 2]])
     assert len(field1) == SIZE
     assert len(field2) == SIZE
     field3 = field1 | field2
     assert len(field3) == SIZE * 2
-    end = time.clock()
-    return end - start
 
 
 def out(a):
@@ -53,6 +83,7 @@ def main():
         out(chunks)
         out(test())
         out(test2())
+        out(test_small())
         print
 
 if __name__ == "__main__":
--- page_size_test/test.sh
+++ page_size_test/test.sh
@@ -10,9 +10,14 @@ cp ../cimpl/field.pyx bf.pyx
 
 cython -2 bf.pyx -o bf.c
 
-gcc -std=c99 -bundle -Os -undefined dynamic_lookup -Wl,-F. -arch x86_64 bf.c \
-	  -I/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
-	  -dynamic -fno-common -fno-strict-aliasing -fwrapv -mno-fused-madd -o bf.so 2>/dev/null
+if [ $(uname) = "Linux" ]; then
+gcc -pthread -shared -fno-strict-aliasing -DNDEBUG -g -fwrapv -O2\
+    -Wstrict-prototypes -fPIC -I/usr/include/python2.7\
+    -Wl,-O1 -Wl,-Bsymbolic-functions -Wl,-Bsymbolic-functions -Wl,-z,relro\
+    bf.c -o bf.so -g
+else
+echo "TODO"
+fi
 
 rm ./field.h
 rm ./bf.pyx
