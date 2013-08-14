e0ac59f8ec253ea28d4a7b5e256a0601c33eec74
Steve Stagg <stestagg@gmail.com>
2013-08-14 17:14:18 +0100

    Add fixes for compiling on Windows

---
+++ cimpl/popcount.h
@@ -0,0 +1,8 @@
+typedef unsigned int usize_t;
+
+int __builtin_popcountl(usize_t v) {
+	v = v - ((v >> 1) & (usize_t)~(usize_t)0/3);                           // temp
+	v = (v & (usize_t)~(usize_t)0/15*3) + ((v >> 2) & (usize_t)~(usize_t)0/15*3);      // temp
+	v = (v + (v >> 4)) & (usize_t)~(usize_t)0/255*15;                      // temp
+	return (usize_t)(v * ((usize_t)~(usize_t)0/255)) >> (sizeof(usize_t) - 1) * CHAR_BIT; // count
+}
