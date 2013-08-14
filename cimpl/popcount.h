6d0edb3b5fef4f04f18bf994610e76bf7d951b73
Steve Stagg <stestagg@gmail.com>
2013-08-14 17:43:17 +0100

    Allow windows compilation to work with Mingw
    Compilation should now work with MSVC and mingw (32-bit)

--- cimpl/popcount.h
+++ cimpl/popcount.h
@@ -1,3 +1,7 @@
+
+#if defined(__MINGW32__)
+#else
+
 typedef unsigned int usize_t;
 
 int __builtin_popcountl(usize_t v) {
@@ -6,3 +10,5 @@ int __builtin_popcountl(usize_t v) {
 	v = (v + (v >> 4)) & (usize_t)~(usize_t)0/255*15;                      // temp
 	return (usize_t)(v * ((usize_t)~(usize_t)0/255)) >> (sizeof(usize_t) - 1) * CHAR_BIT; // count
 }
+
+#endif
